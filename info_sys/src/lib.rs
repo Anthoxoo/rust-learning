05fbda997d6b90d287b85d2edef3a8fbd7c1d827
Antoine <briocheguenot@gmail.com>
2026-02-11 11:08:41 +0100

    added OS and refactored a bit

--- info_sys/src/lib.rs
+++ info_sys/src/lib.rs
@@ -50,11 +50,12 @@ pub mod memory {
             .expect("Not a number.");
 
         let used_mem: u32 = total_mem - free_mem;
+        const KO_TO_MO: u32 = 1024;
 
         return RamInfo {
-            total: total_mem,
-            free: free_mem,
-            used: used_mem,
+            total: total_mem / KO_TO_MO,
+            free: free_mem / KO_TO_MO,
+            used: used_mem / KO_TO_MO,
         };
     }
 }
@@ -65,7 +66,9 @@ pub mod disk {
     pub struct DiskInfo {
         pub total: u64,
         pub free: u64,
+        pub free_percentage: u64,
         pub used: u64,
+        pub used_percentage: u64,
     }
     pub fn get_storage_info() -> DiskInfo {
         let storage_command_output = Command::new("df")
@@ -103,10 +106,65 @@ pub mod disk {
             .parse()
             .expect("Error converting available storage to u64");
 
+        const KB_TO_GO: u64 = 1024 * 1024;
+        let free_disk_pourcentage: u64 = (available_storage * 100) / total_storage;
+        let used_disk_pourcentage: u64 = (used_storage * 100) / total_storage;
+
         return DiskInfo {
-            total: total_storage,
-            free: available_storage,
-            used: used_storage,
+            total: total_storage / KB_TO_GO,
+            free: available_storage / KB_TO_GO,
+            free_percentage: free_disk_pourcentage,
+            used: used_storage / KB_TO_GO,
+            used_percentage: used_disk_pourcentage,
         };
+    }
+}
+
+pub mod os {
+    use std::fs;
+
+    pub fn name() -> String {
+        let path: String = String::from("/etc/os-release");
+        let raw_os_output: String =
+            fs::read_to_string(path).expect("Error reading /etc/os-release.");
+        let classic_name: &str = raw_os_output
+            .lines()
+            .nth(0)
+            .expect("Error reading first line");
+        let mut pretty_name: &str = ""; // no need mut because &str --> on the stack
+
+        for line in raw_os_output.lines() {
+            if line.starts_with("PRETTY_NAME") {
+                pretty_name = line;
+                break;
+            }
+        }
+
+        if pretty_name == r#"PRETTY_NAME="Linux""# {
+            //case where the pretty name has not been changed ( rare case )
+            pretty_name = classic_name
+        }
+
+        pretty_name = pretty_name
+            .split('=')
+            .nth(1)
+            .expect("Error splitting the classic name on the '='")
+            .trim();
+        return pretty_name.to_string();
+    }
+
+    pub fn uptime() -> f64 {
+        let path: String = String::from("/proc/uptime");
+        let raw_uptime_output: String =
+            fs::read_to_string(path).expect("Error reading /proc/uptime.");
+
+        let uptime_output: f64 = raw_uptime_output
+            .split(' ')
+            .nth(0)
+            .expect("Error splitting on the space")
+            .parse()
+            .expect("Error converting to float");
+        const SEC_TO_HOUR: f64 = 60.0 / 60.0;
+        return uptime_output / SEC_TO_HOUR;
     }
 }
