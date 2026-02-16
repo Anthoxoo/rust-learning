2bfc16497e5d4bbfd050cbdc70c9947da2159c81
Antoine <briocheguenot@gmail.com>
2026-02-16 13:45:25 +0100

    little refactoring and improves

--- collection_test/target/debug/deps/collection_test-1f23d286b361a5d8.d
+++ collection_test/target/debug/deps/collection_test-1f23d286b361a5d8.d
@@ -1,5 +1,6 @@
-/home/antoine/Dev/rust-learning/collection_test/target/debug/deps/collection_test-1f23d286b361a5d8.d: src/main.rs
+/home/antoine/Dev/rust-learning/collection_test/target/debug/deps/collection_test-1f23d286b361a5d8.d: src/main.rs src/test1.rs
 
-/home/antoine/Dev/rust-learning/collection_test/target/debug/deps/libcollection_test-1f23d286b361a5d8.rmeta: src/main.rs
+/home/antoine/Dev/rust-learning/collection_test/target/debug/deps/libcollection_test-1f23d286b361a5d8.rmeta: src/main.rs src/test1.rs
 
 src/main.rs:
+src/test1.rs:
--- collection_test/target/debug/deps/collection_test-7d3de6110d1ff48c.d
+++ collection_test/target/debug/deps/collection_test-7d3de6110d1ff48c.d
@@ -1,5 +1,6 @@
-/home/antoine/Dev/rust-learning/collection_test/target/debug/deps/collection_test-7d3de6110d1ff48c.d: src/main.rs
+/home/antoine/Dev/rust-learning/collection_test/target/debug/deps/collection_test-7d3de6110d1ff48c.d: src/main.rs src/test1.rs
 
-/home/antoine/Dev/rust-learning/collection_test/target/debug/deps/libcollection_test-7d3de6110d1ff48c.rmeta: src/main.rs
+/home/antoine/Dev/rust-learning/collection_test/target/debug/deps/libcollection_test-7d3de6110d1ff48c.rmeta: src/main.rs src/test1.rs
 
 src/main.rs:
+src/test1.rs:
