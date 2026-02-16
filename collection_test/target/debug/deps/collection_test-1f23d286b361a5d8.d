455a25ca5ef62e2c1615697d0699f57ab9a9c1cd
Antoine <briocheguenot@gmail.com>
2026-02-16 13:46:21 +0100

    little refactoring and improves[A

--- collection_test/target/debug/deps/collection_test-1f23d286b361a5d8.d
+++
@@ -1,6 +0,0 @@
-/home/antoine/Dev/rust-learning/collection_test/target/debug/deps/collection_test-1f23d286b361a5d8.d: src/main.rs src/test1.rs
-
-/home/antoine/Dev/rust-learning/collection_test/target/debug/deps/libcollection_test-1f23d286b361a5d8.rmeta: src/main.rs src/test1.rs
-
-src/main.rs:
-src/test1.rs:
