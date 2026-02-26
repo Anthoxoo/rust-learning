141b601c84273e47d164968c6ade063146e44e19
Antoine <briocheguenot@gmail.com>
2026-02-26 15:24:05 +0100

    can read and handle errors

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::fs;
+
+use minigrep::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect(); // args() is an iterator and collect() turn what it went through into a vector
+    let config: Config = Config::parse_config(&args).expect("Problem while parsing the arguments");
+
+    run_program(config.file_path);
+}
+
+fn run_program(file_destination: String) {
+    let file_content =
+        fs::read_to_string(file_destination).expect("Couldn't read {file_destination}");
+    println!("{file_content}");
 }
