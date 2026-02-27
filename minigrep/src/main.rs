dabd50e98d1f7e21f6b2f10571e81ee5ce2cbb0e
Antoine <briocheguenot@gmail.com>
2026-02-27 19:00:45 +0100

    expliciter handling of error

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
 use std::fs;
+use std::io::Error;
+use std::process;
 
 use minigrep::*;
 
@@ -7,11 +9,20 @@ fn main() {
     let args: Vec<String> = env::args().collect(); // args() is an iterator and collect() turn what it went through into a vector
     let config: Config = Config::parse_config(&args).expect("Problem while parsing the arguments");
 
-    run_program(config.file_path);
+    let program = run_program(config.file_path);
+    match program {
+        Err(e) => {
+            println!("Error running the command : {e}");
+            process::exit(1);
+        }
+
+        Ok(_) => {}
+    }
 }
 
-fn run_program(file_destination: String) {
+fn run_program(file_destination: String) -> Result<(), Error> {
     let file_content =
         fs::read_to_string(file_destination).expect("Couldn't read {file_destination}");
     println!("{file_content}");
+    Ok(())
 }
