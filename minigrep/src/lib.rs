8631e6bf3d07d3f2fa9b352a13cecd69152dfb45
Antoine <briocheguenot@gmail.com>
2026-02-27 19:01:08 +0100

    v1 of search function

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -5,12 +5,24 @@ pub struct Config {
 
 impl Config {
     pub fn parse_config(args: &[String]) -> Result<Config, &str> {
-        if args.len() > 3 {
-            return Err("Too much argument : 'minigrep [query] [file_path]'");
+        if args.len() > 3 || args.len() <= 1 {
+            // args must looks like ["target/debug/minigrep", "option1", "option2"]
+            return Err("Usage : 'minigrep [query] [file_path]'");
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
 
         return Ok(Config { query, file_path });
     }
+}
+
+pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut line_contains_query: Vec<&str> = Vec::new();
+    for line in content.lines() {
+        if content.contains(query) {
+            line_contains_query.push(line)
+        }
+    }
+
+    return line_contains_query;
 }
